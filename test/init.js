071b12ca2e4ffa9cba029b3b0c69762e1d02afa4
Roman Heinrich <roman.heinrich@gmail.com>
2013-11-24 22:00:15 +0100

    Squashed 'github.com/dowjones/intentionjs/' content from commit 58c1384
    git-subtree-dir: github.com/dowjones/intentionjs
    git-subtree-split: 58c1384f8becc4c532cca5dbf85b490479e59802

---
+++ test/init.js
@@ -0,0 +1,22 @@
+/* global intent: false */
+'use strict';
+function in_init(contexts, callback) {
+  /* jshint validthis: true */
+  var dfds = [];
+  _.each(contexts, function (ctx) {
+    var dfd = $.Deferred();
+    dfds.push(dfd);
+    if (intent.is(ctx)) {
+      dfd.resolve();
+    } else {
+      intent.on(ctx, dfd.resolve);
+    }
+  });
+  if (!contexts || contexts.length === 0) {
+    callback();
+  }
+  $.when.apply(this, dfds).done(callback);
+}
+in_init(['standard'], function () {
+  console.log('standard has been entered');
+});
